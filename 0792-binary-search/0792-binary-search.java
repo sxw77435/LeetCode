9d61d5aa0d06b3885ca49219d546ae31b0345ea2
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-11-19 16:09:35 +0900

    Time: 0 ms (100%), Space: 45.4 MB (77.97%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,16 +1,49 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//        int i = 0 , j = nums.length - 1;
+//        while(i <= j){
+//          int m = (i+j) >>>1;
+//          if(target < nums[m]){
+//             j = m - 1;
+//          }else if(nums[m] < target){
+//             i = m + 1;
+//          }else{
+//             return m;
+//          }
+//        }
+//        return -1;
+//     }
+// }
+// 改动版
+// class Solution {
+//     public int search(int[] nums, int target) {
+//        int i = 0 , j = nums.length;
+//        while(i < j){
+//          int m = (i+j) >>>1;
+//          if(target < nums[m]){
+//             j = m ;
+//          }else if(nums[m] < target){
+//             i = m + 1;
+//          }else{
+//             return m;
+//          }
+//        }
+//        return -1;
+//     }
+// }
+
+//平衡版
 class Solution {
     public int search(int[] nums, int target) {
-       int i = 0 , j = nums.length - 1;
-       while(i <= j){
+       int i = 0 , j = nums.length;
+       while(1 < j - i){
          int m = (i+j) >>>1;
          if(target < nums[m]){
-            j = m - 1;
-         }else if(nums[m] < target){
-            i = m + 1;
-         }else{
-            return m;
+            j = m ;
+         }else {
+            i = m;
          }
        }
-       return -1;
+       return (nums[i] == target) ? i : -1 ;
     }
 }
