9ac61c100d296900194dfafbff30ba5c70b692f5
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-11-19 16:34:45 +0900

    Time: 0 ms (100%), Space: 42.8 MB (54.8%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,20 +1,39 @@
+// class Solution {
+//     public int searchInsert(int[] nums, int target) {
+//         int low  = 0;
+//         int high = nums.length - 1;
+
+//         while(low <= high){
+//             int mid = (low + high) >>>1;
+//             long midVal = nums[mid];
+
+//             if(midVal < target){
+//                 low = mid + 1;
+//             }else if( target < midVal){
+//                 high = mid - 1;
+//             }else{
+//                 return mid;
+//             }
+//         }
+//         return low;
+//     }
+// }
+
+//leftmost
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int low  = 0;
-        int high = nums.length - 1;
+        int i  = 0;
+        int j = nums.length - 1;
 
-        while(low <= high){
-            int mid = (low + high) >>>1;
-            long midVal = nums[mid];
+        while(i <=  j ){
+            int m = (i + j) >>>1;
 
-            if(midVal < target){
-                low = mid + 1;
-            }else if( target < midVal){
-                high = mid - 1;
+            if(target <= nums[m]){
+                j = m - 1;
             }else{
-                return mid;
+                i = m + 1;
             }
         }
-        return low;
+        return i;
     }
 }
