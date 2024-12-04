0cdf6aa2952195f2c3d7ca79d55ca33645f54663
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-04 09:55:29 +0900

    Time: 1 ms (100%), Space: 47.1 MB (56.39%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,5 +1,21 @@
-
 class Solution {
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] result = new int[n];
+        int i = 0, j = n - 1;
+        for (int p = n - 1; p >= 0; p--) {
+            if (Math.abs(A[i]) > Math.abs(A[j])) {
+                result[p] = A[i] * A[i];
+                i++;
+            } else {
+                result[p] = A[j] * A[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}
+/*class Solution {
     public int[] sortedSquares(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] * nums[i];
@@ -7,4 +23,4 @@ class Solution {
         Arrays.sort(nums);
         return nums;
     }
-}
+}*/
