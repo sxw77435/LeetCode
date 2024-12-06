85147dee592ff42131270f07dd50ac96ddc60514
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-06 15:48:46 +0900

    Time: 2 ms (77.3%), Space: 62.1 MB (6.92%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -20,20 +20,25 @@
 //     }
 // }
 
-// 第一种
+//第二种
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyPrice = prices[0];
-        int profit = 0;
+        // 初始化最低买入价格为无穷大，最大利润为 0
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
 
-        for (int i = 1; i < prices.length; i++) {
-            if (buyPrice > prices[i]) {
-                buyPrice = prices[i];
+        // 遍历数组
+        for (int price : prices) {
+            // 更新最低买入价格
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                // 计算当前利润并更新最大利润
+                maxProfit = price - minPrice;
             }
-
-            profit = Math.max(profit, prices[i] - buyPrice);
         }
 
-        return profit;        
+        return maxProfit; // 返回最大利润
     }
 }
+
