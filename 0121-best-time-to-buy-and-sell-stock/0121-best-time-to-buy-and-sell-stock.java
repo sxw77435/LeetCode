deb5073c6e7a6bf6a9e95a472d4c1f0fca76a588
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-06 15:49:26 +0900

    Time: 2 ms (77.3%), Space: 62.2 MB (6.92%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,41 +1,22 @@
 // 第一种
-//class Solution {
-//     public int maxProfit(int[] prices) {
-//         //最低买入价格，初始化为prices[0]
-//         int buy = prices[0];
-//         //利润也初始化为0
-//         int profit = 0;
-
-//         //从第二天开始遍历，
-//         for(int i = 1; i < prices.length; i++){
-//             //如果当天价格更低，则更新买入最低价格
-//             if(prices[i] < buy){
-//                 buy = prices[i];
-//             //如果当天利润更改，则更新利润
-//             }else if(prices[i] - buy > profit){
-//                 profit = prices[i] - buy;
-//             }
-//         }
-//         return profit;
-//     }
-// }
-
-//第二种
 class Solution {
     public int maxProfit(int[] prices) {
-        int lsf = Integer.MAX_VALUE;
-        int op = 0;
-        int pist = 0;
-        
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
+        //最低买入价格，初始化为prices[0]
+        int buy = prices[0];
+        //利润也初始化为0
+        int profit = 0;
+
+        //从第二天开始遍历，
+        for(int i = 1; i < prices.length; i++){
+            //如果当天价格更低，则更新买入最低价格
+            if(prices[i] < buy){
+                buy = prices[i];
+            //如果当天利润更改，则更新利润
+            }else if(prices[i] - buy > profit){
+                profit = prices[i] - buy;
             }
         }
-        return op;
+        return profit;
     }
 }
+
