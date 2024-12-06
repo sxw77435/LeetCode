56fc758248f20878d6b1db2f4b41767ba0c4aef6
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-06 14:39:49 +0900

    Time: 0 ms (100%), Space: 42.5 MB (12.03%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,11 +1,31 @@
 //注意，这两个数组本身就是从小到大的数组，之后都储存到nums1中，顺序仍然是从小到大
+
+//第一种方式
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         //这里的j是nums2的索引值，之后从nums的第m位索引开始添加到nums1中
+//         for(int j = 0, i = m; j < n; j++){
+//             nums1[i] = nums2[j];
+//             i++;
+//         }
+
+//         Arrays.sort(nums1);
+//     }
+// }
+
+//第二种方式
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int j = 0, i = m; j < n; j++){
-            nums1[i] = nums2[j];
-            i++;
-        }
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        Arrays.sort(nums1);
+        while(j >= 0){
+            if(i >= 0 && nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k --] = nums2[j--];
+            }
+        }
     }
 }
