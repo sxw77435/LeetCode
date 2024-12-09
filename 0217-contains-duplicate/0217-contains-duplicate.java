f87160450df6b28be764f0072a5e66841fe0e5b2
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-09 13:29:31 +0900

    Time: 10 ms (89.11%), Space: 58.3 MB (32.05%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,30 +1,31 @@
 //第一种：哈希表
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true; // 如果已经存在，返回 true
+            }
+            seen.add(num); // 如果不存在，加入哈希表
+        }
+
+        return false; // 遍历结束，无重复
+    }
+}
+
+//第二种：排序法
 // class Solution {
 //     public boolean containsDuplicate(int[] nums) {
-//         HashSet<Integer> seen = new HashSet<>();
+//         Arrays.sort(nums); // 对数组排序
 
-//         for (int num : nums) {
-//             if (seen.contains(num)) {
-//                 return true; // 如果已经存在，返回 true
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] == nums[i - 1]) { // 检查相邻元素是否相同
+//                 return true;
 //             }
-//             seen.add(num); // 如果不存在，加入哈希表
 //         }
 
 //         return false; // 遍历结束，无重复
 //     }
 // }
 
-//第二种：排序法
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums); // 对数组排序
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) { // 检查相邻元素是否相同
-                return true;
-            }
-        }
-
-        return false; // 遍历结束，无重复
-    }
-}
