bc2b2833a6b43946ad34a48a9e9f1a7829678bb3
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-09 12:51:52 +0900

    Time: 2 ms (78%), Space: 53.5 MB (11.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,49 @@
+//第一种解法
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Arrays.sort(nums);
+//         return nums[nums.length / 2];
+//     }
+// }
+//第二种解法
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n = nums.length;
+//         Map<Integer, Integer> map = new HashMap<>();
+        
+//         for (int i = 0; i < n; i++) {
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//         }
+        
+//         n = n / 2;
+//         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             if (entry.getValue() > n) {
+//                 return entry.getKey();
+//             }
+//         }
+        
+//         return 0;
+//     }
+// }
+
+//第三种
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int count = 0;
+        int candidate = 0;
+        
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
     }
 }
