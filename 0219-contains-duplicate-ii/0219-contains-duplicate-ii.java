b859de21cba5d7e2315efd18dd8ca2bdcbb0dff2
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-09 14:50:44 +0900

    Time: 19 ms (53.7%), Space: 58.3 MB (19.77%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,20 +1,13 @@
 //哈希表
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // 使用哈希表存储元素及其最新的索引
-        HashMap<Integer, Integer> map = new HashMap<>();
-
+       HashMap<Integer, Integer> map = new HashMap<>(); // 用于存储元素及其索引
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                // 检查索引差是否满足条件
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                }
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true; // 发现满足条件的重复元素
             }
-            // 更新当前元素的索引
-            map.put(nums[i], i);
+            map.put(nums[i], i); // 更新索引值
         }
-
-        return false; // 如果没有找到，返回 false
+        return false; // 遍历结束后无重复
     }
 }
