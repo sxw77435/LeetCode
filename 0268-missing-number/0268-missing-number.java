a5bc8b876b19875218d10f39a2e30c747107df03
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-09 15:21:22 +0900

    Time: 0 ms (100%), Space: 45.3 MB (50.45%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -17,35 +17,35 @@
 
 //第二种方法： 异或法
 //最优解法
-// class Solution {
-//     public int missingNumber(int[] nums) {
-//         int n = nums.length;
-//         int xor = 0;
-
-//         for (int i = 0; i < n; i++) {
-//             xor ^= i;       // 异或索引
-//             xor ^= nums[i]; // 异或元素
-//         }
-
-//         xor ^= n; // 最后还要与 n 异或
-//         return xor;
-//     }
-//}
-
-//第三种 排序法
-import java.util.Arrays;
-
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums); // 对数组进行排序
+        int n = nums.length;
+        int xor = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i) {
-                return i; // 索引与值不一致，返回索引
-            }
+        for (int i = 0; i < n; i++) {
+            xor ^= i;       // 异或索引
+            xor ^= nums[i]; // 异或元素
         }
 
-        return nums.length; // 如果没有缺失，说明缺失的是 n
+        xor ^= n; // 最后还要与 n 异或
+        return xor;
     }
 }
+
+//第三种 排序法
+// import java.util.Arrays;
+
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         Arrays.sort(nums); // 对数组进行排序
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != i) {
+//                 return i; // 索引与值不一致，返回索引
+//             }
+//         }
+
+//         return nums.length; // 如果没有缺失，说明缺失的是 n
+//     }
+// }
 
