47c0ec729d5e970ac7bce93aa367de91dc6d1183
송흔위 <149069669+sxw77435@users.noreply.github.com>
2024-12-27 10:08:49 +0900

    Time: 9 ms (55.6%), Space: 46 MB (23.28%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -1,33 +1,28 @@
-import java.util.*;
-
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        // 创建结果数组
-        String[] result = new String[score.length];
-
-        // 创建一个索引数组，存储每个得分的原始索引
-        Integer[] indices = new Integer[score.length];
-        for (int i = 0; i < score.length; i++) {
-            indices[i] = i;
-        }
-
-        // 根据得分对索引数组进行排序（从高到低）
-        Arrays.sort(indices, (a, b) -> score[b] - score[a]);
-
-        // 分配名次
-        for (int i = 0; i < indices.length; i++) {
-            int index = indices[i];
+        
+        int n = score.length;
+        
+        int[][] sortedPairs = new int[n][2];
+        for (int i = 0 ; i < n ; i++) sortedPairs[i] = new int[] {i, score[i]};
+        
+        Arrays.sort(sortedPairs, (x, y) -> (y[1] - x[1]));
+        
+        String[] ans = new String[n];
+        
+        for (int i = 0 ; i < n ; i++) {
+            
             if (i == 0) {
-                result[index] = "Gold Medal";
+                ans[sortedPairs[i][0]] = "Gold Medal";
             } else if (i == 1) {
-                result[index] = "Silver Medal";
+                ans[sortedPairs[i][0]] = "Silver Medal";
             } else if (i == 2) {
-                result[index] = "Bronze Medal";
+                ans[sortedPairs[i][0]] = "Bronze Medal";
             } else {
-                result[index] = String.valueOf(i + 1);
+                ans[sortedPairs[i][0]] = String.valueOf(i+1);
             }
         }
-
-        return result;
+        
+        return ans;
     }
 }
