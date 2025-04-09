8cd83f28cf393180417a174ee02edce4d7d1d7a4
송흔위 <149069669+sxw77435@users.noreply.github.com>
2025-04-09 20:17:15 +0900

    Time: 25 ms (94.48%), Space: 51.2 MB (92.71%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,38 +1,33 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums); // Step 1: 排序
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Step 2: 跳过重复的 nums[i]
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
 
-            int target = -nums[i]; // 我们要找两个数，使它们的和为 -nums[i]
-            int left = i + 1;
-            int right = nums.length - 1;
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
 
-            // Step 3: 双指针查找
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-                if (sum == target) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // Step 4: 跳过重复的 left 和 right
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } else if (sum < target) {
-                    left++;
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
                 } else {
-                    right--;
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
                 }
             }
         }
-
-        return result;
+        return res;        
     }
 }
