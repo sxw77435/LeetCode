7961b8760dd21fb9359e833d7e2d383cc5a16730
송흔위 <149069669+sxw77435@users.noreply.github.com>
2025-04-09 19:49:21 +0900

    Time: 31 ms (58.34%), Space: 51.9 MB (36.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,38 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Step 1: 排序
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.length - 1;
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Step 2: 跳过重复的 nums[i]
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
+            int target = -nums[i]; // 我们要找两个数，使它们的和为 -nums[i]
+            int left = i + 1;
+            int right = nums.length - 1;
 
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
+            // Step 3: 双指针查找
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Step 4: 跳过重复的 left 和 right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
                 } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+                    right--;
                 }
             }
         }
-        return res;        
+
+        return result;
     }
 }
